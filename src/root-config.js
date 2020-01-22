55e8850c0f9ce8f7d8fa862749eb9b17995e3e53
Joel Denning <joeldenning@gmail.com>
2020-01-21 18:00:21 -0700

    Fixes

--- src/root-config.js
+++ src/root-config.js
@@ -15,8 +15,21 @@ registerApplication(
 
 registerApplication(
   "@polyglot-mf/account-settings",
-  () => System.import("@polyglot-mf/account-settings"),
+  () => loadWithoutAmd("@polyglot-mf/account-settings"),
   isActive.accountSettings
 );
 
 start();
+
+// A lot of angularjs libs are compiled to UMD, and if you don't process them with webpack
+// the UMD calls to window.define() can be problematic.
+function loadWithoutAmd(name) {
+  return Promise.resolve().then(() => {
+    let globalDefine = window.define;
+    delete window.define;
+    return System.import(name).then(module => {
+      window.define = globalDefine;
+      return module;
+    });
+  });
+}
