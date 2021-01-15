2d156fff069e55d0d18c324dbfbef07709429a02
Joel Denning <joeldenning@gmail.com>
2021-01-15 14:48:58 -0700

    Upgrade to webpack 5 and c-s-s@2

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (env) => ({
-  entry: path.resolve(__dirname, "src/root-config"),
-  output: {
-    filename: "polyglot-mf-root-config.js",
-    libraryTarget: "system",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devtool: "sourcemap",
-  module: {
-    rules: [
-      { parser: { system: false } },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }],
-      },
-    ],
-  },
-  devServer: {
-    historyApiFallback: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "polyglot-mf";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  const merge = webpackMerge({
+    customizeArray: webpackMerge.unique(
+      "plugins",
+      ["HtmlWebpackPlugin"],
+      (plugin) => plugin.constructor && plugin.constructor.name
+    ),
+  });
+
+  return merge(
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: "src/index.ejs",
+          templateParameters: {
+            isLocal: webpackConfigEnv && webpackConfigEnv.isLocal === "true",
+            orgName,
+          },
+        }),
+      ],
     },
-    disableHostCheck: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: "src/index.ejs",
-      templateParameters: {
-        isLocal: env && env.isLocal === "true",
-      },
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  externals: ["single-spa", /^@polyglot-mf\/.+$/],
-});
+    defaultConfig,
+    {
+      // modify the webpack config however you'd like to by adding to this object
+    }
+  );
+};
